16a35713c37a9411cc0bad132180dd481e9f2364
Daniel Farr <danielcfarr@gmail.com>
2024-08-31 13:47:15 -0600

    ready for shop page

--- web/src/views/ShopPage.tsx
+++ web/src/views/ShopPage.tsx
@@ -1,9 +1,5 @@
 import PaymentMethodWizard from "../components/stripe/PaymentMethodWizard";
 
 export default function ShopPage() {
-    return (
-        <div className="container">
-            <PaymentMethodWizard />
-        </div>
-    );
+    return <div className="container"></div>;
 }
