aff844bae313715d065eddc3082ffd734da4cdb1
Daniel Farr <danielcfarr@gmail.com>
2024-09-05 21:45:47 -0600

    ready for signup

--- web/src/components/modal/LoginFormModal.tsx
+++ web/src/components/modal/LoginFormModal.tsx
@@ -3,6 +3,7 @@ import LoginForm, { LoginFormProps } from "../user/LoginForm";
 import useUserStore from "../../stores/useUserStore";
 import React from "react";
 import { UseModalCreateProps } from "../../hooks/useModal";
+import { Link } from "react-router-dom";
 
 type LoginFormModalProps = UseModalCreateProps;
 
@@ -19,11 +20,26 @@ export default function LoginFormModal(props: LoginFormModalProps) {
         []
     );
 
+    const onClickSignup = (e: React.MouseEvent<HTMLAnchorElement>) => {
+        e.preventDefault();
+        console.log("show signup!");
+    };
+
     return (
         <Modal {...props}>
             <Modal.Header>Login to Bid!</Modal.Header>
             <Modal.Body className="p-0 rounded-b-lg">
                 <LoginForm onSubmit={onSubmit} />
+                <div className="text-center py-4 mb-2">
+                    <div className="mb-4 font-semibold">OR</div>
+                    <Link
+                        className="text-purple-700 hover:underline"
+                        to="#"
+                        onClick={onClickSignup}
+                    >
+                        Create Account
+                    </Link>
+                </div>
             </Modal.Body>
         </Modal>
     );
