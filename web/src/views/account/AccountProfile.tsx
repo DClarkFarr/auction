183a3ca86c82d718841c5c7b09ef578b7ce2c4f4
Daniel Farr <danielcfarr@gmail.com>
2024-09-14 15:20:49 -0600

    change payment method

--- web/src/views/account/AccountProfile.tsx
+++ web/src/views/account/AccountProfile.tsx
@@ -7,10 +7,19 @@ import ResetPasswordForm, {
 import UserService from "../../services/UserService";
 import useToastContext from "../../providers/useToastContext";
 import { AxiosError } from "axios";
+import CardDetails from "../../components/stripe/CardDetails";
+import { Link } from "react-router-dom";
+import React from "react";
+import StripeProvider from "../../providers/StripeProvider";
+import StripeCardForm from "../../components/stripe/StripeCardForm";
 
 export default function AccountProfile() {
-    const { user } = useUserStore();
+    const { user, paymentMethod } = useUserStore();
     const { toast } = useToastContext();
+
+    const [paymentView, setPaymentView] = React.useState<"form" | "card">(
+        "card"
+    );
 
     if (!user) {
         return null;
@@ -40,6 +49,14 @@ export default function AccountProfile() {
         }
     };
 
+    const onCardSaved = () => {
+        toast({
+            text: "Payment method saved",
+            type: "success",
+        });
+        setPaymentView("card");
+    };
+
     return (
         <div className="account-profile">
             <div className="container">
@@ -60,6 +77,52 @@ export default function AccountProfile() {
                 <Panel title="Reset password">
                     <ResetPasswordForm onSubmit={onSubmitPassword} />
                 </Panel>
+
+                <div className="h-10"></div>
+
+                {paymentMethod && (
+                    <Panel title="Payment Method">
+                        {paymentView === "card" && (
+                            <div className="mb-4">
+                                <h3 className="font-semibold text-gray-800 mb-1">
+                                    Saved Card On File
+                                </h3>
+                                <CardDetails
+                                    paymentMethod={paymentMethod}
+                                    actions={
+                                        <>
+                                            <Link
+                                                to=""
+                                                className="text-purple-600 hover:text-purple-700 hover:underline"
+                                                onClick={(e) => (
+                                                    setPaymentView("form"),
+                                                    e.preventDefault()
+                                                )}
+                                            >
+                                                Edit
+                                            </Link>
+                                        </>
+                                    }
+                                />
+                            </div>
+                        )}
+                        {paymentView === "form" && (
+                            <>
+                                <h3 className="mb-2 text-xl font-semibold">
+                                    Default Payment Method
+                                </h3>
+                                <p className="mb-10">
+                                    You will not be charged now. After you have
+                                    won your bids, you select winning bids and
+                                    complete the purchase with the card on file.
+                                </p>
+                                <StripeProvider initWithSetup>
+                                    <StripeCardForm onSuccess={onCardSaved} />
+                                </StripeProvider>
+                            </>
+                        )}
+                    </Panel>
+                )}
             </div>
         </div>
     );
